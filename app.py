a681407ea38c52c6b745afc16d08db3a837dc5ea
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-10 18:55:11 -0500

    create database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from application.controller.controllers import app
 from application.controller.controllers import db
 
+from application.model.Club import Club
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True, host='127.0.0.1', port=5000)
