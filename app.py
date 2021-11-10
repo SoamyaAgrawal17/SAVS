f7e57c12f31e6cf49ef7996a93390966a706154a
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-10 18:49:41 -0500

    create database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from application.controller.controllers import app
+from application.controller.controllers import db
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host='127.0.0.1', port=5000)
