36ca438a4c07be2c381131a91cfa5d437e3430b3
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-11 22:02:02 -0500

    Creating entities

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ from application.model.StudentEvents import StudentEvents
 from application.model.Students import Students
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host='127.0.0.1', port=5000)
