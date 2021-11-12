da1c591442c1ed729ce234595fc5abe656368563
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-11 21:52:01 -0500

    Adding new entities

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from application.controller.controllers import app
 from application.controller.controllers import db
 
 from application.model.Club import Club
+from application.model.Events import Events
+from application.model.Role import Role
+from application.model.StudentEvents import StudentEvents
+from application.model.Students import Students
 
 if __name__ == '__main__':
     db.create_all()
