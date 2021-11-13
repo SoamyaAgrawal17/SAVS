7782eb51435b6e9b16e8de2ab119e099a50c666a
ShikhaAsrani <shikhaasrani@shikhas-MacBook-Pro.local>
2021-11-13 00:23:08 -0500

    edit club test6

--- application/service/ClubService.py
+++ application/service/ClubService.py
@@ -24,7 +24,7 @@ def edit_club(db, club_id, club_details):
     print(club_id)
     metadata = db.MetaData()
     clubs = db.Table('club',metadata)
-    club = db.select([clubs]).where(clubs._id == club_id)
+    club = db.select([clubs]).where(clubs.id == club_id)
     print(club)
     # club = db.club.query.get(_id = club_id)
     print(club_details)    
