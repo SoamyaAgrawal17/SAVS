107ad645f9fadef2b3e9d6b4c3597aaaa4725916
ShikhaAsrani <shikhaasrani@shikhas-MacBook-Pro.local>
2021-11-12 23:18:01 -0500

    changing url

--- application/controller/controllers.py
+++ application/controller/controllers.py
@@ -11,7 +11,7 @@ log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uoaidyfadwrwpe:72aae551e395c9450d8611d0e0dc52856ca98b4e9b22b4cbada9b4bb894d653c@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d1e9sornll6622'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uoaidyfadwrwpe:72aae551e395c9450d8611d0e0dc52856ca98b4e9b22b4cbada9b4bb894d653c@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d1e9sornll6622'
 db = SQLAlchemy(app)
 
 @app.route('/events', methods=['GET'])
