d01b0546b27802b2daec8dff5a8d41e74ec3ffd7
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-14 14:19:26 -0500

    Updating postgres url

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import flask_sqlalchemy, os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ktmornyueendxp:bfa6586b37549edd37ce1096c7c1563ac48f3358448981ee04dd4ee0816f4083@ec2-44-193-182-0.compute-1.amazonaws.com:5432/d9uolnhsqlmh31'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/savs_database"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xemmxmxrzwwqml:7a22441f69138467943cd7212dd0186ce219bc424589ebdf9eef8fbe08ea4af3@ec2-44-194-225-27.compute-1.amazonaws.com:5432/d76kto02mv7hb3'
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
