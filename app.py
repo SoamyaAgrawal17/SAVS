81319108143abcd1e61d80b7cdd1b4707dbfedd2
Soamya Agrawal <soamyaagrawal@gmail.com>
2021-11-14 15:29:47 -0500

    Fixing database url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ktmornyueendxp:bfa6586b37549edd37ce1096c7c1563ac48f3358448981ee04dd4ee0816f4083@ec2-44-193-182-0.compute-1.amazonaws.com:5432/d9uolnhsqlmh31'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ktmornyueendxp:bfa6586b37549edd37ce1096c7c1563ac48f3358448981ee04dd4ee0816f4083@ec2-44-193-182-0.compute-1.amazonaws.com:5432/d9uolnhsqlmh31'
 
 
 blueprints = [
