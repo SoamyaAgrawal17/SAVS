b3c2639b9e6fa3e9200c661230cef4d01f656ea2
ShikhaAsrani <shikhaasrani@shikhas-MacBook-Pro.local>
2021-11-15 12:00:56 -0500

    merging code for club head

--- application/controller/ClubsController.py
+++ application/controller/ClubsController.py
@@ -2,6 +2,8 @@ import logging
 import json
 
 from application.service import ClubService
+from application.service import StudentService
+from application.model.Role import Role
 from flask import Blueprint
 from flask import request, Response
 
@@ -35,14 +37,58 @@ def get_club_by_id(club_id):
 
 @mod.route('/clubs/<club_id>', methods=['PUT'])
 def edit_club(club_id):
-    inputs = request.args
-    res = ClubService.edit_club(club_id, inputs)
+    data = request.get_json()
+    email_id = data["emailId"]
+    if email_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    club_information = data["club"]
+    student_id = StudentService.get_id(email_id)
+    if student_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    query = Role.query.filter_by(club_id=club_id, student_id=student_id)
+    result = query.first()
+    if result is None or result.role != "Club Head":
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    res = ClubService.edit_club(club_id, club_information)
     rsp = Response(res, status=200, content_type="text/plain")
     return rsp
 
 
 @mod.route('/clubs/<club_id>', methods=['DELETE'])
 def delete_club(club_id):
+    data = request.get_json()
+    email_id = data["emailId"]
+    if email_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    student_id = StudentService.get_id(email_id)
+    if student_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    query = Role.query.filter_by(club_id=club_id, student_id=student_id)
+    result = query.first()
+    if result is None or result.role != "Club Head":
+        return Response("Invalid Request", status=200, content_type="text/plain")
     res = ClubService.delete_club(club_id)
     rsp = Response(res, status=200, content_type="text/plain")
+    return rsp
+
+
+@mod.route('/member/<club_id>', methods=['PUT'])
+def add_member(club_id=None):
+    data = request.get_json()
+    email_id = data["emailId"]
+    student_email_id = data["student_email_id"]
+    if email_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    editor_student_id = StudentService.get_id(email_id)
+    if editor_student_id is None:
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    student_id = StudentService.get_id(student_email_id)
+    if student_id is None:
+        return Response("error: student isn't registered", status=200, content_type="text/plain")
+    query = Role.query.filter_by(club_id=club_id, student_id=editor_student_id)
+    result = query.first()
+    if result is None or result.role != "Club Head":
+        return Response("Invalid Request", status=200, content_type="text/plain")
+    member = ClubService.add_member(club_id, student_id)
+    rsp = Response(member, status=200, content_type="application/JSON")
     return rsp
--- application/service/ClubService.py
+++ application/service/ClubService.py
@@ -1,6 +1,7 @@
 from application.utilities.database import db
 from application.model.Club import Club
 from application.model.Role import Role
+from application.model.Student import Student
 
 
 def get_id(club_name):
@@ -23,11 +24,18 @@ def get_clubs():
 def get_club(club_id):
     query = db.session.query(Club).filter(Club._id.in_([club_id]))
     result = query.first()
+    if result is None:
+        return None
     return result.as_dict()
 
 
-def edit_club(club_id,club_details):
-    club = get_club(club_id)
+def edit_club(club_id, club_details):
+    # update club
+    club = Club.query.get(club_id)
+    print(club)
+    if 'name' in club_details.keys():
+        if club_details['name'] == '':
+            return "club cannot have empty name"
     for key,value in club_details.items():
         setattr(club,key,value)
     db.session.commit() 
@@ -36,8 +44,21 @@ def edit_club(club_id,club_details):
 
 def delete_club(club_id):
     # delete club
-    # Club.query.filter_by(_id=club_id).delete()
-    club = get_club(club_id)
-    db.session.delete(club)
+    club = Club.query.filter_by(_id=club_id)
+    if club is None:
+        return "club does not exist"
+    Club.query.filter_by(_id=club_id).delete()
     db.session.commit()
     return "club deleted"
+
+
+def add_member(club_id, student_id):
+    student = Student.query.get(student_id)
+    if student is None:
+        return "error: student not found"
+    club = Club.query.get(club_id)
+    if club is None:
+        return "error: club not found"
+    db.session.add(Role(student_id=student_id, club_id=club_id, role="Club Member"))
+    db.session.commit()
+    return "Club member added"
