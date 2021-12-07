ff1d770854746efddf16c431350ac251903b8592
vanijain123 <65841491+vanijain123@users.noreply.github.com>
2021-12-07 16:32:44 -0500

    csrf

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 from application.controller import ClubsController, EventsController
 from application.controller import StudentsController, DatabaseController
 from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 CORS(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xbasblmhnpkibi:8a1264b9c4b71b4c8abac23f12fc7a991e3fe81671b1169a0d09c6692f7606f4@ec2-18-207-72-235.compute-1.amazonaws.com:5432/d1qeu6i6agoejb'
@@ -21,12 +24,6 @@ for bp in blueprints:
 
 db.init_app(app)
 
-'''
-from application.model import Club, Event, Role, Student, StudentEvent
-with app.app_context():
-    db.create_all()
-    db.session.commit()
-'''
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
